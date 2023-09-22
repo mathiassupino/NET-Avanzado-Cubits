44c3f2edb61614bd99625f0aeb5441cd2b45b693
Mathias Supino <mathisupino@gmail.com>
2023-09-22 15:44:03 -0300

    Agrego controllador Users

---
+++ NET.Avanzado/NET.Avanzado/Controllers/UsersController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NET.Avanzado.Models;
+
+namespace NET.Avanzado.Controllers
+{
+    [Route("users")]
+    public class UsersController : Controller
+    {
+        [Route("")]
+        public ActionResult Index()
+        {
+            var user = new UserViewModel
+            {
+                Name = "Mathias Supino",
+                Email = "mathisupino@gmail.com"
+            };
+
+            return View(user);
+        }
+    }
+}
