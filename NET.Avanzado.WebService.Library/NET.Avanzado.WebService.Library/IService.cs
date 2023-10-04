3e3e5f410c71e3dbc2a1b0ab285986f00a2f7cf0
Mathias Supino <mathisupino@gmail.com>
2023-10-04 12:40:59 -0300

    Agregando WCF

---
+++ NET.Avanzado.WebService.Library/NET.Avanzado.WebService.Library/IService.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.Serialization;
+using System.ServiceModel;
+
+namespace NET.Avanzado.WebService.Library
+{
+    [ServiceContract]
+    public interface IService
+    {
+        [OperationContract]
+        Office GetOfficeData();
+    }
+
+    [DataContract]
+    public class Office
+    {
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public string Address { get; set; }
+    }
+}
---
+++ NET.Avanzado.WebService.Library/NET.Avanzado.WebService.Library/Service.cs
@@ -0,0 +1,14 @@
+﻿namespace NET.Avanzado.WebService.Library
+{
+    public class Service : IService
+    {
+        public Office GetOfficeData()
+        {
+            return new Office
+            {
+                Name = "Cubits SRL",
+                Address = "Fanny Jacovsky 3539, San Francisco, Córdoba"
+            };
+        }
+    }
+}
