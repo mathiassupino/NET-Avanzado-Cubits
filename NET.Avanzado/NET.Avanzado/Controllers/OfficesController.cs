bd6ad85a2f5672475d77fc29aff6ea4e2f4959ef
Mathias Supino <mathisupino@gmail.com>
2023-10-04 12:59:15 -0300

    Controlador office

---
+++ NET.Avanzado/NET.Avanzado/Controllers/OfficesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServiceReference1;
+
+namespace NET.Avanzado.Controllers
+{
+    [Route("offices")]
+    public class OfficesController : Controller
+    {
+        [Route("")]
+        public async Task<ActionResult> Index()
+        {
+            var officeService = new ServiceClient();
+
+            var office = await officeService.GetOfficeDataAsync();
+
+            return View(office);
+        }
+    }
+}
